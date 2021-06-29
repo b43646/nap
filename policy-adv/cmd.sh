3d38f622a8779c990157c8d085bfd750bf133e03
b43646 <408514942@qq.com>
2021-06-29 11:25:32 +0000

    add policy

--- policy-adv/cmd.sh
+++ policy-adv/cmd.sh
@@ -1,2 +1,3 @@
 docker rm -f app-protect
-docker run -dit --name app-protect -p 80:80 -v /root/nap/policy-adv/nginx.conf:/etc/nginx/nginx.conf -v /root/nap/policy-adv/policy_base.json:/etc/nginx/policy/policy_base.json -v /root/nap/policy-adv/policy_mongo_linux_JSON.json:/etc/nginx/policy/policy_mongo_linux_JSON.json  app-protect:20210316
+docker run -dit --name app-protect -p 80:80 -v /root/nap/policy-adv/nginx.conf:/etc/nginx/nginx.conf -v /root/nap/policy-adv/policy_base.json:/etc/nginx/policy/policy_base.json -v /root/nap/policy-adv/policy_mongo_linux_JSON.json:/etc/nginx/policy/policy_mongo_linux_JSON.json  app-protect:tc
+#docker run -dit --name app-protect -p 80:80 -v /root/nap/policy-adv/nginx.conf:/etc/nginx/nginx.conf -v /root/nap/policy-adv/policy_base.json:/etc/nginx/policy/policy_base.json -v /root/nap/policy-adv/policy_mongo_linux_JSON.json:/etc/nginx/policy/policy_mongo_linux_JSON.json  app-protect:20210316
