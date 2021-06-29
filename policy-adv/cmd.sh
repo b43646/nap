3bc8b5972188f4c3769b0f47996804467490b7a8
b43646 <408514942@qq.com>
2021-06-29 18:43:52 +0800

    Update cmd.sh

--- policy-adv/cmd.sh
+++ policy-adv/cmd.sh
@@ -1,1 +1,2 @@
-123
+docker rm -f app-protect
+docker run -dit --name app-protect -p 80:80 -v /home/ubuntu/policy-adv/nginx.conf:/etc/nginx/nginx.conf -v /home/ubuntu/policy-adv/policy_base.json:/etc/nginx/policy/policy_base.json -v /home/ubuntu/policy-adv/policy_mongo_linux_JSON.json:/etc/nginx/policy/policy_mongo_linux_JSON.json  app-protect:20210316
